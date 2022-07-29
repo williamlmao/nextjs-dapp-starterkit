ffbf2045711803244f2dfe994bc3a2a1dda0d41d
Will <willliuwillliu@gmail.com>
2022-07-29 14:28:02 -0400

    Updated marketing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,17 @@ import { NextPageWithLayout } from "./_app";
 
 const Page: NextPageWithLayout = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="text-center">
-        <h1 className="text-6xl font-extrabold">
-          A starterkit for all your dapping needs.
+    <div className="flex flex-col items-center justify-center h-full p-4">
+      <div className="text-center max-w-[700px]">
+        <h1 className="text-4xl sm:text-6xl font-extrabold">
+          Dapp Starterkit Marketing Page
         </h1>
         <p className="my-4 text-xl">
           This starterkit installs and configures various libraries, components,
-          layouts, and animations so that you can get to building faster.
+          layouts, and animations so that you can get to building faster. Put
+          your marketing page here
         </p>
+        <p className="my-4 text-xl">Put your marketing page here</p>
       </div>
       <Link href="/dashboard">
         <Button color="primary">Launch the app</Button>
